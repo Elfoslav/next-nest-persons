62e46936503f83dc7c027d640bf8e506de4e5cdb
Tomáš Hromník <tomas.hromnik@woltair.cz>
2023-07-08 10:07:16 +0200

    Add cors headers in server/vercel.json

--- client/src/app/lib/apiClient.ts
+++ client/src/app/lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // Replace with your API base URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export default apiClient;
